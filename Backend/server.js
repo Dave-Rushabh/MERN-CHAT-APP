f99cbe841376d200c47ba1c2c516840aca3e47fc
dave-rushabh <rushabh.dave24@gmail.com>
2022-08-04 22:55:57 +0530

    server updated

--- Backend/server.js
+++ Backend/server.js
@@ -6,11 +6,20 @@ const dotenv = require("dotenv");
 const app = express();
 dotenv.config();
 
+// tackle CORS
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
+
 // defining variables
 const PORT = process.env.PORT || 7000;
 
 // defining AIPs
-app.get("/", (req, res) => {
+app.get("/dummy", (req, res) => {
   res.send("API is running");
 });
 
