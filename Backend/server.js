c0df1fae3848d3841c6983c2c68c76f88f62781f
dave-rushabh <rushabh.dave24@gmail.com>
2022-08-05 22:27:08 +0530

    backend updated with data models

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,9 @@
-// imports
 const express = require("express");
 const dotenv = require("dotenv");
 
-// helper functions or methods
 const app = express();
 dotenv.config();
 
-// tackle CORS
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
@@ -15,13 +12,10 @@ app.use(function (req, res, next) {
   next();
 });
 
-// defining variables
 const PORT = process.env.PORT || 7000;
 
-// defining AIPs
 app.get("/dummy", (req, res) => {
   res.send("API is running");
 });
 
-// server will listen to the requests in following manner
 app.listen(PORT, console.log("started server on port 5000"));
